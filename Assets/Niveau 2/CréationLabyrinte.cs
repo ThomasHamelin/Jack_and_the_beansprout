a83270d25f14a491583835d73ecb6bee215c81ac
CanadianRocketDude <fxbordeleau@gmail.com>
2023-02-09 16:35:51 -0500

    créé des fichiers pour la génération du labyrinte du niveau 2

---
+++ Assets/Niveau 2/CréationLabyrinte.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CréationLabyrinte : MonoBehaviour
+{
+    [SerializeField] GameObject coin1;
+    [SerializeField] GameObject coin2;
+    [SerializeField] GameObject coin3;
+    [SerializeField] GameObject coin4;
+
+
+    [SerializeField] float longueur;
+    [SerializeField] float largeur;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
