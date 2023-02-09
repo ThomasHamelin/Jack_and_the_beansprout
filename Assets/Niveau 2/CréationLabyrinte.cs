307cc2ca8366ce88e8d00b3248959da9013cb6a7
CanadianRocketDude <fxbordeleau@gmail.com>
2023-02-09 17:02:12 -0500

    sauvegarde script faits

--- Assets/Niveau 2/CréationLabyrinte.cs
+++ Assets/Niveau 2/CréationLabyrinte.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CréationLabyrinte : MonoBehaviour
@@ -9,14 +11,20 @@ public class CréationLabyrinte : MonoBehaviour
     [SerializeField] GameObject coin3;
     [SerializeField] GameObject coin4;
 
-
     [SerializeField] float longueur;
     [SerializeField] float largeur;
+
+    [SerializeField] float CoordonneDepartX;
+    [SerializeField] float CoordonneDepartY;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        coin1.transform.position = new Vector2(CoordonneDepartX, CoordonneDepartY);
+        coin2.transform.position = new Vector2(coin1.transform.position.x + longueur ,coin1.transform.position.y);
+        coin3.transform.position = new Vector2(coin1.transform.position.x, coin1.transform.position.y + largeur);
+        coin4.transform.position = new Vector2(coin1.transform.position.x + longueur, coin1.transform.position.y + largeur);
     }
 
     // Update is called once per frame
