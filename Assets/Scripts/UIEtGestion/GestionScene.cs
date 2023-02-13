23e412e0deb9f810b34f40a76e7d0ef468aa74c0
DaphneeBlais <124931851+DaphneeBlais@users.noreply.github.com>
2023-02-13 11:32:11 -0500

    UI
    Début création UI instruction, travail sur la scene d'accueil

--- Assets/Scripts/UIEtGestion/GestionScene.cs
+++ Assets/Scripts/UIEtGestion/GestionScene.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GestionScene : MonoBehaviour
 {
+    //[SerializeField] private TextMeshProUGUI _txtReadyJ1 = default;
+    //[SerializeField] private TextMeshProUGUI _txtReadyJ2 = default;
+    [SerializeField] private GameObject _uiInstruct = default;
+
     /*
      * Rôle : Charger la scene suivante
      * Entrée : Aucune
@@ -13,6 +18,14 @@ public class GestionScene : MonoBehaviour
     public void ChangerScene()
     {
         int indexSceneCourante = SceneManager.GetActiveScene().buildIndex;
+        
+        switch(indexSceneCourante)
+        {
+            case 0:
+                _uiInstruct.SetActive(true);
+                break;
+        }
+
         SceneManager.LoadScene(indexSceneCourante + 1);
     }
 
