e4e41afc0538712f2b8ad097eb59432fa3ca8d8a
DaphneeBlais <124931851+DaphneeBlais@users.noreply.github.com>
2023-02-13 09:58:37 -0500

    Correction bug avec gestion points

--- Assets/Scripts/UIEtGestion/GestionPoints.cs
+++ Assets/Scripts/UIEtGestion/GestionPoints.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class GestionPoints : MonoBehaviour
 {
@@ -30,12 +33,12 @@ public class GestionPoints : MonoBehaviour
         if (p_numJoueur == 1)
         {
             _scoreJ1 += p_points;
-            _txtScoreJ1 = "Points : " + _scoreJ1;
+            _txtScoreJ1.text = "Points : " + _scoreJ1.ToString();
         }
         else if (p_numJoueur == 2)
         {
             _scoreJ2 += p_points;
-            _txtScoreJ2 = "Points : " + _scoreJ1;
+            _txtScoreJ2.text = "Points : " + _scoreJ1.ToString();
         }
 
     }
