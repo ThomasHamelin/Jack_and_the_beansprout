2b9e42a3e04d3af6ea8a6b590d72fdb9db13e3ac
DaphneeBlais <124931851+DaphneeBlais@users.noreply.github.com>
2023-03-27 11:30:39 -0400

    Split screen niveau 2 complété

--- Assets/Niveau 2/JoueurNiv2.cs
+++ Assets/Niveau 2/JoueurNiv2.cs
@@ -1,13 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class JoueurNiv2 : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI _txtDonneDepart = default;
-    [SerializeField] float _moveSpeed = 600f;
-    [SerializeField] float _rotationSpeed = 700f;
+    [SerializeField] private float _moveSpeed = 600f;
+    [SerializeField] private float _rotationSpeed = 700f;
     private Rigidbody2D _rb;
     private Vector2 _direction;
     bool _jeuDebute = false;
@@ -15,73 +13,69 @@ public class JoueurNiv2 : MonoBehaviour
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        StartCoroutine(DonnerDepart());
     }
 
     void FixedUpdate()
     {
-        if (_jeuDebute)
+        if (_jeuDebute) //Quand le niveau aura commencé
         {
             MouvementsJoueurs();
             RotateInDirectionOfInput();
         }
     }
 
+    /*
+     * Rôle : Déplacer le joueur dans la direction voulue
+     * Entrée : Aucune
+     */
     private void MouvementsJoueurs()
     {
-        if (this.CompareTag("Player1"))
+        if (this.CompareTag("Player1")) //Si c'est le joueur 1
         {
-            float posX = Input.GetAxisRaw("Horizontal_P1");
-            float posY = Input.GetAxisRaw("Vertical_P1");
-            _direction = new Vector2(posX, posY) * _moveSpeed * Time.deltaTime;
+            float posX = Input.GetAxisRaw("Horizontal_P1"); //Regarde dans quelle direction horizontale le joueur veut se déplacer
+            float posY = Input.GetAxisRaw("Vertical_P1"); //Regarde dans quelle direction verticale le joueur veut se déplacer
+            _direction = new Vector2(posX, posY) * _moveSpeed * Time.deltaTime; //Détermine le vecteur de direction
         }
-        else if (this.CompareTag("Player2"))
+        else if (this.CompareTag("Player2")) //Si c'est le joueur 2
         {
-            float posX = Input.GetAxis("Horizontal_P2");
-            float posY = Input.GetAxis("Vertical_P2");
-            _direction = new Vector2(posX, posY) * _moveSpeed * Time.deltaTime;
+            float posX = Input.GetAxis("Horizontal_P2"); //Regarde dans quelle direction horizontale le joueur veut se déplacer
+            float posY = Input.GetAxis("Vertical_P2"); //Regarde dans quelle direction verticale le joueur veut se déplacer
+            _direction = new Vector2(posX, posY) * _moveSpeed * Time.deltaTime; //Détermine le vecteur de direction
         }
 
-        _rb.velocity = _direction;
+        _rb.velocity = _direction; //Déplace le joueur dans la direction voulue
     }
 
+    /*
+     * Rôle : Faire tourner le joueur dans la direction de son déplacement
+     * Entrée : Aucune
+     */
     private void RotateInDirectionOfInput()
     {
-            if (_direction != Vector2.zero)
+            if (_direction != Vector2.zero) //Si le joueur se déplace
             {
-                _rb.freezeRotation = false;
-                Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _direction);
-                Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+                _rb.freezeRotation = false; //On permet la rotation
+                Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _direction); //On détermine dans quelle direction effectuer la rotation
+                Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime); //On crée quaternion de déplacement
 
-                _rb.MoveRotation(rotation);
+                _rb.MoveRotation(rotation); //On effectue la rotation
             }
-            else
+            else //Si le joueur ne se déplace pas
             {
-                _rb.freezeRotation = true;
+                _rb.freezeRotation = true; //On bloque la rotation
             }
         
     }
 
-    IEnumerator DonnerDepart()
+    /*
+     * Rôle : Indiquer que le joueur peut commencer à se déplacer
+     * Entrée : Aucune
+     * Sortie : Aucune
+     */
+    public void DebuterJeu()
     {
-        _jeuDebute = false;
-        _txtDonneDepart.gameObject.SetActive(false);
-        yield return new WaitForSeconds(3f);
-        if(this.CompareTag("Player1"))
-        {
-            _txtDonneDepart.gameObject.SetActive(true);
-
-        }
-        _txtDonneDepart.text = "3";
-        yield return new WaitForSeconds(1f);
-        _txtDonneDepart.text = "2";
-        yield return new WaitForSeconds(1f);
-        _txtDonneDepart.text = "1";
-        yield return new WaitForSeconds(1f);
-        _txtDonneDepart.text = "Partez!";
         _jeuDebute = true;
-        yield return new WaitForSeconds(1f);
-        _txtDonneDepart.gameObject.SetActive(false);
     }
+
 
 }
