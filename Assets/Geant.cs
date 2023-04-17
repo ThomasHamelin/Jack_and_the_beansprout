3f179cd891886813ec9cdc6269ff3b184045b642
CanadianRocketDude <fxbordeleau@gmail.com>
2023-04-17 09:21:59 -0400

    j'ai pas dormi pis je devient fou

--- Assets/Geant.cs
+++ Assets/Geant.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Data;
 using TMPro;
 using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEditor;
 using UnityEngine;
 using static System.Net.WebRequestMethods;
 
@@ -13,18 +15,24 @@ public class Geant : MonoBehaviour
     [SerializeField] private float _VitesseCourse;
     [SerializeField] private float _distanceVision;
     [SerializeField] private float _rotationSpeed;
-    private Rigidbody2D _rb;
-  
-    private int _newPos;
+    [SerializeField] private GameObject _yeux;
+    [SerializeField] private GameObject _geant;
+
+    private int _rand;
     private float[] _UniteMouvement = new float[4];
-    private int _ancienneposition;
     
+   
+    private float _VecteurX;
+    private float _VecteurY;
+
+    private float _VecteurGaucheX;
+    private float _VecteurGaucheY;
 
     // Start is called before the first frame update
     void Start()
     {
-        _rb = GetComponent<Rigidbody2D>();
-      
+
+
     }
     public void PartageDonnées(float Donné, int x)
     {
@@ -34,119 +42,35 @@ public class Geant : MonoBehaviour
     //Update is called once per frame
     void Update()
     {
-        // 0 = up, 1 = down , 2 = left, 3 = right
-        _newPos = Random.Range(0, 4);
-        RaycastHit2D hitup = Physics2D.Raycast(transform.position, Vector2.up, _UniteMouvement[3], LayerMask.GetMask("DetectionMur"));
-        RaycastHit2D hitdown = Physics2D.Raycast(transform.position, Vector2.down, _UniteMouvement[3], LayerMask.GetMask("DetectionMur"));
-        RaycastHit2D hitleft = Physics2D.Raycast(transform.position, Vector2.left, _UniteMouvement[2], LayerMask.GetMask("DetectionMur"));
-        RaycastHit2D hitright = Physics2D.Raycast(transform.position, Vector2.right, _UniteMouvement[2], LayerMask.GetMask("DetectionMur"));
+        
 
-        switch (_newPos)
-        {
-            case 0:
-                if(_ancienneposition != 1)
-                {
-                    if(hitup == false)
-                    {
-                        if(hitleft != true)
-                        {
-                            _ancienneposition = 2;
-                        }
-                        if(hitright != true)
-                        {
-                            _ancienneposition = 3;
-                        }
-                        else
-                        {
-                            _ancienneposition = 0;
-                        }
-                    }
-                    else
-                    {
-                        _ancienneposition = 1;
-                    }
-                }
-                break;
-            case 1:
-                if (_ancienneposition != 0)
-                {
-                    if (hitdown == false)
-                    {
-                        if (hitleft != true)
-                        {
-                            _ancienneposition = 2;
-                        }
-                        if (hitright != true)
-                        {
-                            _ancienneposition = 3;
-                        }
-                        else
-                        {
-                            _ancienneposition = 1;
-                        }
-                    }
-                    else
-                    {
-                        _ancienneposition = 0;
-                    }
-                }
-                break;
-            case 2:
-                if (_ancienneposition != 3)
-                {
-                    if (hitleft == false)
-                    {
-                        if (hitup != true)
-                        {
-                            _ancienneposition = 0;
-                        }
-                        if (hitdown != true)
-                        {
-                            _ancienneposition = 1;
-                        }
-                        else
-                        {
-                            _ancienneposition = 2;
-                        }
-                    }
-                    else
-                    {
-                        _ancienneposition = 3;
-                    }
-                }
-                break;
-            case 3:
-                if (_ancienneposition != 2)
-                {
-                    if (hitright == false)
-                    {
-                        if (hitdown != true)
-                        {
-                            _ancienneposition = 1;
-                        }
-                        if (hitup != true)
-                        {
-                            _ancienneposition = 0;
-                        }
-                        else
-                        {
-                            _ancienneposition = 3;
-                        }
-                    }
-                    else
-                    {
-                        _ancienneposition = 2;
-                    }
-                }
-                break;
+        _VecteurX = 10 * Mathf.Cos(Mathf.Deg2Rad * transform.localRotation.eulerAngles.z);
+        _VecteurY = 10 * Mathf.Sin(Mathf.Deg2Rad *transform.localRotation.eulerAngles.z);
+
+        _VecteurX += _geant.transform.position.x;
+        _VecteurY += _geant.transform.position.y;
+
+        
+        Vector3 dir = new Vector3(_VecteurX, _VecteurY , 0);
 
 
-        }
+        _VecteurX = 10 * Mathf.Cos(Mathf.Deg2Rad * transform.localRotation.eulerAngles.z);
+        _VecteurY = 10 * Mathf.Sin(Mathf.Deg2Rad * transform.localRotation.eulerAngles.z);
+
+        _VecteurX += _geant.transform.position.x;
+        _VecteurY += _geant.transform.position.y;
+
+
+        Vector3 dirGauche = new Vector3(_VecteurX, _VecteurY, 0);
+
+        RaycastHit2D hitDevant = Physics2D.Raycast(transform.position, dir, _UniteMouvement[3], LayerMask.GetMask("DetectionMur"));
+        RaycastHit2D hitGauche = Physics2D.Raycast(transform.position, dirGauche, _UniteMouvement[3], LayerMask.GetMask("DetectionMur"));
+
+        Debug.DrawLine(transform.position, dir, Color.red);
+        Debug.DrawLine(transform.position, dirGauche, Color.blue);
 
 
     }
-
-
 }
 
 
