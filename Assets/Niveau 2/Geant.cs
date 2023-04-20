3ccad567a4ffed737bb5e1345ace17bee1dd3a35
CanadianRocketDude <fxbordeleau@gmail.com>
2023-04-20 18:03:49 -0400

    coroutines sont horribles (┬┬﹏┬┬)

--- Assets/Niveau 2/Geant.cs
+++ Assets/Niveau 2/Geant.cs
@@ -7,6 +7,7 @@ using Unity.VisualScripting;
 using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static System.Net.WebRequestMethods;
 using static UnityEngine.GraphicsBuffer;
 
@@ -20,10 +21,10 @@ public class Geant : MonoBehaviour
     [SerializeField] private GameObject _yeux;
     [SerializeField] private GameObject _geant;
 
-  
+
     private float[] _UniteMouvement = new float[4];
-    
-   
+
+
     private float _VecteurX;
     private float _VecteurY;
 
@@ -31,12 +32,13 @@ public class Geant : MonoBehaviour
     private float _VecteurDroiteY;
 
 
-    private bool _yes = false;
 
+
+    Coroutine _confirmMovement;
     // Start is called before the first frame update
     void Awake()
     {
-        _yes = true;
+
 
     }
     public void PartageDonnées(float Donné, int x)
@@ -48,143 +50,179 @@ public class Geant : MonoBehaviour
     void Update()
     {
 
-        if (_yes == true)
+
+
+
+        _VecteurX = 5 * Mathf.Cos(Mathf.Deg2Rad * (transform.eulerAngles.z));
+        _VecteurY = 5 * Mathf.Sin(Mathf.Deg2Rad * (transform.eulerAngles.z));
+
+
+
+
+        Vector2 dir = new Vector3(_VecteurX + transform.position.x, _VecteurY + transform.position.y);
+
+
+        _VecteurDroiteX = 5 * Mathf.Cos(Mathf.Deg2Rad * (transform.eulerAngles.z - 90));
+        _VecteurDroiteY = 5 * Mathf.Sin(Mathf.Deg2Rad * (transform.eulerAngles.z - 90));
+
+
+
+
+
+
+        Vector2 dirDroite = new Vector3(_VecteurDroiteX + transform.position.x, _VecteurDroiteY + transform.position.y);
+
+
+        Debug.DrawLine(transform.position, dir, Color.red);
+        Debug.DrawLine(transform.position, dirDroite, Color.green);
+
+
+        RaycastHit2D hitDevant = Physics2D.Raycast(this.transform.position, transform.TransformDirection(Vector2.right), _UniteMouvement[2], LayerMask.GetMask("DetectionMur"));
+        RaycastHit2D hitDroite = Physics2D.Raycast(this.transform.position, transform.TransformDirection(Vector2.down), _UniteMouvement[3], LayerMask.GetMask("DetectionMur"));
+
+        StopAllCoroutines();
+
+        if (hitDevant == false && hitDroite == false)
         {
-            _VecteurX = 5 * Mathf.Cos(Mathf.Deg2Rad * (transform.eulerAngles.z));
-            _VecteurY = 5 * Mathf.Sin(Mathf.Deg2Rad * (transform.eulerAngles.z));
+            //dir = -1
+            //confirm =1
+            StopAllCoroutines();
+            StartCoroutine(Bouge(-1, 1));
 
-
-
-
-            Vector2 dir = new Vector3(_VecteurX + transform.position.x, _VecteurY + transform.position.y);
-
-
-            _VecteurDroiteX = 5 * Mathf.Cos(Mathf.Deg2Rad * (transform.eulerAngles.z-90));
-            _VecteurDroiteY = 5 * Mathf.Sin(Mathf.Deg2Rad * (transform.eulerAngles.z-90));
-
-
-
-
-
-
-            Vector2 dirDroite = new Vector3(_VecteurDroiteX + transform.position.x, _VecteurDroiteY + transform.position.y);
-
-
-            Debug.DrawLine(transform.position, dir, Color.red);
-            Debug.DrawLine(transform.position, dirDroite, Color.green);
-
-
-            RaycastHit2D hitDevant = Physics2D.Raycast(this.transform.position, transform.TransformDirection(Vector2.right), _UniteMouvement[2], LayerMask.GetMask("DetectionMur"));
-            RaycastHit2D hitDroite = Physics2D.Raycast(this.transform.position, transform.TransformDirection(Vector2.down), _UniteMouvement[3], LayerMask.GetMask("DetectionMur"));
-
-
-            
-            if (hitDevant == false && hitDroite == false)
-            {
-                //StartCoroutine(Bouge(1, -1));
-                turn(-1);
-                move(1);
-            }
-            if (hitDevant ==false && hitDroite == true)
-            {
-                //StartCoroutine(Bouge(1, 0));
-                turn(0);
-                move(1);
-            }
-            if (hitDevant == true && hitDroite == false)
-            {
-                //StartCoroutine(Bouge(0, 1));
-                turn(-1);
-                move(1);
-            }
-            if (hitDevant == true && hitDroite == true)
-            {
-                //StartCoroutine(Bouge(0, 1));
-                turn(1);
-                move(0);
-            }
-            
-            
         }
+        if (hitDevant == false && hitDroite == true)
+        {
+            //dir = 0
+            //confirm =1
+            StopAllCoroutines();
+            StartCoroutine(Bouge(0, 1));
+        }
+        if (hitDevant == true && hitDroite == false)
+        {
+            //dir = -1
+            //confirm =1
+            StopAllCoroutines();
+            StartCoroutine(Bouge(-1, 1));
+        }
+        if (hitDevant == true && hitDroite == true)
+        {
+            //dir = 1
+            
+            StopAllCoroutines();
+            StartCoroutine(Bouge(1, 0));
+        }
+
+
+
     }
-    IEnumerator Rotate(int dir)
-    {
-        turn(dir);
-        yield return null;
-    }
-    IEnumerator Bouge(int dir, int moveCondition) 
+    IEnumerator Bouge(int dir, int Confirm)
     {
         yield return StartCoroutine(Rotate(dir));
 
-        move(moveCondition);
-        yield return null;
+        yield return StartCoroutine(avance(Confirm));
+
+        yield break;
     }
+    //IEnumerator Rotate(int dir)
+    //{
 
-    void turn(int dir)
+    //    float angleT = transform.eulerAngles.z + (dir * 90);
+    //    angleT = angleT % 360; 
+    //    Quaternion rotationTarget = new Quaternion(0, 0, angleT, 0);
+
+    //    Quaternion rotationBase = new Quaternion(0, 0, transform.eulerAngles.z, 0);
+
+    
+    //    if (dir == 0)
+    //    {
+    //        yield break;
+    //    }
+    //    do
+    //    {
+    //        transform.rotation = Quaternion.Slerp(transform.rotation, rotationTarget, _rotationSpeed * Time.deltaTime);
+
+    //        if (transform.rotation.z == rotationBase.z)
+    //        {
+    //            yield break;
+    //        }
+
+
+
+    //    } while (transform.eulerAngles.z != rotationBase.z);
+
+
+
+
+    //}
+
+    IEnumerator avance(int moveCondition)
     {
-       
         
-       //dir = 1: clockwise
-       //dir = -1: anti-clockwise;
-        Quaternion rotationTarget = new Quaternion(0, 0, transform.localEulerAngles.z + (dir * 90f),0);
-
-
-        float startRotation = transform.eulerAngles.y;
-        
-        float t = 0.0f;
-        while (t < _rotationSpeed)
-        {
-            t += Time.deltaTime;
-
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationTarget, Time.deltaTime * _rotationSpeed);
-
-        }
-        
-
-    }
-    void move(int MovingConfirm)
-    {
-
         int angle = (int)(transform.eulerAngles.z % 360);
+        int i = 0;
+        int y = 0;
 
-        if (MovingConfirm == 1)
+        if (moveCondition == 1)
         {
-            int i = 0;
-            int y = 0;
+
 
             if (angle == 180)
-            { 
+            {
                 i = -1;
             }
-            else if(angle == 0)
+            else if (angle == 0)
             {
                 i = 1;
             }
-            else { 
-                i = 0; 
+            else
+            {
+                i = 0;
             }
 
             if (angle == 270)
             {
-                y = -1; 
+                y = -1;
             }
-            else if(angle == 90) 
+            else if (angle == 90)
             {
-                y = 1; 
+                y = 1;
             }
             else
             {
                 y = 0;
             }
-            Vector3 target = new Vector3(transform.position.x + (_UniteMouvement[0] * i), transform.position.y + (_UniteMouvement[1] * y), 0);
-            //do
-            //{
-            transform.position = new Vector3(target.x, target.y, 0);
-
-            //} while (transform.position.x != target.x && transform.position.y != target.y);
         }
+        Vector3 target = new Vector3(transform.position.x + (_UniteMouvement[0] * i), transform.position.y + (_UniteMouvement[1] * y), 0);
+
+        while (this.transform.rotation.z != target.z)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, target, _VitesseMarche * Time.deltaTime);
+            yield return null;
+        }
+        this.transform.position = target;
+        yield return null;
+
+
     }
 
+    IEnumerator Rotate(float dir)
+    {
+        float angleT = transform.eulerAngles.z + (dir * 90);
+        angleT = angleT % 360;
+        Quaternion rotationTarget = new Quaternion(0, 0, angleT, 0);
+
+        Quaternion rotationBase = new Quaternion(0, 0, transform.eulerAngles.z, 0);
+        while (this.transform.rotation.z != rotationTarget.z)
+        {
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rotationTarget, 3f * Time.deltaTime);
+            yield return null;
+        }
+        this.transform.rotation = Quaternion.Euler(0f, 0f, angleT);
+        yield return null;
+
+
+
+    }
 }
 
 
