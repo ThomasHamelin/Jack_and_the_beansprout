3a2b981f72474d1b1b57671051aa03fe7a8a5d8d
EmmanuelleCloutier <darkforest1045@gmail.com>
2023-04-26 17:38:00 -0400

    now tbk

--- Assets/Niveau 3/fallingTree.cs
+++ Assets/Niveau 3/fallingTree.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class fallingTree : MonoBehaviour
 {
     public float finishedFallingHeight = -35f;
+    public GameObject _poussiere;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = _poussiere.GetComponent<Animator>();
+    }
 
     public void fall()
     {
         this.GetComponent<BoxCollider2D>().isTrigger = true;
+        StartCoroutine(explosion());
     }
+
+    IEnumerator explosion()
+    {
+        yield return new WaitForSecondsRealtime(8f);
+        anim.SetBool("chutevigne", true);
+
+    }
+    
 }
